3e08eac92a194a834e172e47995688fcca527e2a
Priya <priyaillikka@gmail.com>
2024-11-24 16:29:36 +0300

    feature change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>hello
+      <div>Feature
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
